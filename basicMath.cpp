05575125ddc350ca7ac96be8606aae2deb73d400
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-09-20 14:34:51 +0000

    Added program

---
+++ basicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright Cristiano 2022
+//
+// Created by Cristiano Sellitto
+// Created in September 2022
+// A program that finds the area and perimeter or a rectangle
+
+#include <iostream>
+
+int main() {
+    std::cout << "A rectangle is 5cm by 3cm" << std::endl;
+    std::cout << "\nThis rectangle's area is " << (5 * 3) << "cm²" << std::endl;
+    std::cout << "This rectangle's perimeter is ";
+    std::cout << (2 * (5 + 3)) << "cm" << std::endl;
+
+    std::cout << "\nDone." << std::endl;
+}
